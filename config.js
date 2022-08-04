2737e5fc4546c3dd62765275e5e23080c2de76ad
XzenzBotz <hapeadvan1404@gmail.com>
2022-08-04 18:32:12 +0700

    Baru-hahah

---
+++ config.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api Nya Bng
+global.APIs = {
+	zenz: 'https://api.lolhuman.xyz',
+}
+
+// Udah Free Apikey Jadi Klo Mau Ubah Silahkan
+global.APIKeys = {
+	'https://api.lolhuman.xyz': 'azzbot',
+}
+
+// Other
+global.namabot = ['XznBotz-MD']
+global.namaowner = ['Its Me Akmalོ']
+global.owner = ['6281511480762']
+global.premium = ['6281511480762']
+global.packname = 'XznBot-MD'
+global.author = 'WhatsApp•Bot'
+global.sessionName = 'Xavior'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: 'Donee Kak ~',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Jadikan Bot Admin Terlebih Dahulu!',
+    owner: 'Khusus Owner Bot',
+    group: 'Itu Khusus Grup Bre...!',
+    private: 'Cuma Bisa Chat Pribadi Bre...',
+    bot: 'Khusus Nomor Bot',
+    wait: '⏳ Waitt...',
+    endLimit: 'Limit Harian Sudah Habis, Limit Akan Direset Besok',
+}
+//Kalo Ngasih Limit Jangan Dikit Lah Bng
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/xavi.jpg')
+global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
